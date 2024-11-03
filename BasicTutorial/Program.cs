d86f8f0972ddebd48eba5a1fc4290ef51cbb90a0
abusafayatarefin92 <abusafayatarefin92@gmail.com>
2024-11-03 12:14:18 +0600

    2024 11 03

---
+++ BasicTutorial/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+TypeCasting typeCasting = new TypeCasting();
+int x = typeCasting.TypeCastingMethod();
+int y = typeCasting.TypeCastingMethod2();
+
+var testVar = "noob";
+
+Console.WriteLine(testVar + " " + x + y);
